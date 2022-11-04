3af2920e4202df045d70cb0cf62ad01fa163a32a
Rishi kumar prajapati <100562380+Rishipraja@users.noreply.github.com>
2022-11-04 08:57:14 +0530

    Add files via upload

---
+++ student grade.py
@@ -0,0 +1,22 @@
+# student gread
+x=int(input("enter the gread = "))
+if(x>90):
+    print("A+ grade")
+elif(x>80 and x<90):
+    print("A grade")
+elif(x>70 and x<80):
+    print("B+ grade")
+elif(x>60 and x<70):
+    print("B grade")
+elif(x>50 and x<60):
+    print("C+ grade")
+elif(x>40 and x<50):
+    print("C grade")
+elif(x>30 and x<40):
+    print("D+ grade")    
+elif(x>20 and x<30):
+    print("D grade")
+elif(x>10 and x<20):
+    print("E+ grade")
+elif(x>0 and x<10):
+    print("E grade")
